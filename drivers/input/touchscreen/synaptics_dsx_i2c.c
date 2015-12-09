81d0961eb336192ff25eb55a85af6d24ca026b91
flar2 <asegaert@gmail.com>
2015-12-08 22:42:37 -0500

    wake_gestures: Option to temporarily disable with power key

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -6665,7 +6665,7 @@ static int synaptics_dsx_panel_cb(struct notifier_block *nb,
 		/* for in-cell design touch solutions */
 		if (event == FB_EARLY_EVENT_BLANK) {
 #ifdef CONFIG_WAKE_GESTURES
-			gestures_enabled = (s2w_switch > 0) || (dt2w_switch > 0);
+			gestures_enabled = ((s2w_switch > 0) || (dt2w_switch > 0)) && (!pwrkey_pressed);
 #endif
 			if (*blank != FB_BLANK_POWERDOWN)
 				return 0;
@@ -6932,6 +6932,7 @@ static int synaptics_rmi4_resume(struct device *dev)
 					rmi4_data->board;
 
 #ifdef CONFIG_WAKE_GESTURES
+	pwrkey_pressed = 0;
 	if (gestures_enabled) {
 		s2w_enable(rmi4_data, false);
 		goto out;
