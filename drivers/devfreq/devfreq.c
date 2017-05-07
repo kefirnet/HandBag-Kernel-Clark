1af64285075d6f9db771d04c839464a4b7b5ea0c
Sultanxda <sultanxda@gmail.com>
2017-05-07 13:23:20 -0700

    PM / devfreq: Boost "f9824900.sdhc" and "soc:qcom,cpubw" on screen wake
    This makes our SD storage controller use its maximum clock and our DDR bus
    use its maximum bandwidth when the screen is turned on.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -45,6 +45,8 @@ static DEFINE_MUTEX(devfreq_list_lock);
 
 /* List of devices to boost when the screen is woken */
 static const char *boost_devices[] = {
+	"f9824900.sdhc",
+	"soc:qcom,cpubw",
 	NULL
 };
 
