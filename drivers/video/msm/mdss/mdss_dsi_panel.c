c4cbef94f1fa72d3bda46de6966f3580b655b3a4
Nathan Chancellor <natechancellor@gmail.com>
2017-05-10 22:22:50 -0700

    lazyplug: Only use super aggressive profile when screen is off
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -50,6 +50,8 @@ static int onboot = true;
 
 DEFINE_LED_TRIGGER_GLOBAL(bl_led_trigger);
 
+extern void lazyplug_enter_lazy(bool enter, bool video);
+
 bool display_on = true;
 
 bool is_display_on(void)
@@ -849,6 +851,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	}
 
 	display_on = true;
+	lazyplug_enter_lazy(false, false);
 
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
@@ -979,6 +982,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds);
 
 	display_on = false;
+	lazyplug_enter_lazy(true, false);
+
 
 #ifdef CONFIG_POWERSUSPEND
 	set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
